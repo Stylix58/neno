fa06e0c5000a2f23c9815488689f18d880bd383e
Stylix58 <63605602+Stylix58@users.noreply.github.com>
2021-01-08 00:51:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,21 +12,26 @@ async def on_ready() :
     await client.change_presence(status = discord.Status.idle, activity = discord.Game("Multi-tasks bot - n!"))
     print("I am online")
 
-@client.command()
-async def ping(ctx) :
-    """Check the ping time of the bot"""
-    await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
+class General(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def ping(ctx) :
+        """Check the ping time of the bot"""
+        await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
 
-@client.command()
-async def clear(ctx, amount=3) :
-    """Purge the indicated amount of messages (by default 3)"""
-    await ctx.channel.purge(limit=amount)
+    @commands.command()
+    async def clear(ctx, amount=3) :
+        """Purge the indicated amount of messages (by default 3)"""
+        await ctx.channel.purge(limit=amount)
 
-@client.command()
-async def ping_service(ctx, url):
-    """Ping a url"""
-    await ctx.send(f"Pinging the URL...")
-    response = requests.get(url)
-    await ctx.send(f"The URL have return " + response.status_code + " code")
+    @commands.command()
+    async def ping_service(ctx, url):
+        """Ping a url"""
+        await ctx.send(f"Pinging the URL...")
+        response = requests.get(url)
+        await ctx.send(f"The URL have return " + response.status_code + " code")
 
+bot.add_cog(Greetings(client))
 client.run(token)
