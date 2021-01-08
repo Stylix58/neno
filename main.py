873a9e254d59388f39d042d0035ec0ae57817fec
Stylix58 <63605602+Stylix58@users.noreply.github.com>
2021-01-08 01:37:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import discord
 from discord.ext import commands
 from os import getenv
-from uptime import monitor_uptime
 
 client = commands.Bot(command_prefix="nn!")
 token = getenv("DISCORD_TOKEN")
 
 @client.event
 async def on_ready() :
-    await client.change_presence(status=discord.Status.online, activity = discord.Game(name = "Multi-tasks bot - nn!"))
+    await client.change_presence(status = discord.Status.online, activity = discord.Game("Multi-Tasks Bot - nn! - "))
 
 @client.command()
 async def ping(ctx) :
@@ -19,14 +18,5 @@ async def ping(ctx) :
 async def clear(ctx, amount=3) :
     """Purge the indicated amount of messages (by default 3)"""
     await ctx.channel.purge(limit=amount)
-
-@client.command()
-async def uptime(ctx, url) :
-    """Check the uptime of a URL"""
-    await ctx.send(f"Checking the URL...")
-    if monitor_uptime(url) == True:
-        await ctx.send(f"✔ The URL is up!")
-    else:
-        await ctx.send(f"❌ The URL is down!")
     
 client.run(token)
