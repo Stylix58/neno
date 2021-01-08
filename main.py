9a44e0b79588e3b0879a58100cf1fee3b90a6751
Stylix58 <63605602+Stylix58@users.noreply.github.com>
2021-01-08 07:18:37 +0100

    ok

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ async def clear(ctx, amount=3) :
 async def check_url(ctx, url):
     """Ping a URL for check his activity"""
     try:
+        if not "http://" or "https://" in url:
+            url = "http://" + url
         await ctx.send(f"The URL have return the code " + str(requests.get(url).status_code) + "!")
     except:
         await ctx.send(f"We have a problem with this URL...")
