5dd343fee08cf0cb123036e9a340a21da3eb28a6
Stylix58 <63605602+Stylix58@users.noreply.github.com>
2021-01-08 06:51:15 +0100

    adding command uptime

--- coms/general.py
+++ coms/general.py
@@ -1,3 +1,8 @@
+import discord
+from discord.ext import commands
+import requests
+client = commands.Bot()
+
 @client.command()
 async def ping(ctx) :
     """Check the ping time of the bot"""
@@ -7,3 +12,12 @@ async def ping(ctx) :
 async def clear(ctx, amount=3) :
     """Purge the indicated amount of messages (by default 3)"""
     await ctx.channel.purge(limit=amount)
+
+@client.command()
+async def uptime(ctx, url):
+    """Ping a URL for check his uptime"""
+    r = requests.get(url)
+    if r.status_code == 200:
+        await ctx.send(f"⏫ The URL is up! (HTTP code: " + str(r.status_code) + ")")
+    else:
+        await ctx.send(f"⏬ The URL is down! (HTTP code: " + str(r.status_code) + ")")
