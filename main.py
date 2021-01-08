727b1452b542a72f212690897816f4e16afec14b
Stylix58 <63605602+Stylix58@users.noreply.github.com>
2021-01-08 18:14:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,40 +3,38 @@ import requests
 from discord.ext import commands
 from os import getenv
 
-bot = commands.Bot(command_prefix="nn!", help_command=None)
+# init the bot
+
+bot = commands.Bot(command_prefix="nn!")
 token = getenv("DISCORD_TOKEN")
 
+# set status config
 @bot.event
 async def on_ready():
     await bot.change_presence(status = discord.Status.online, activity = discord.Game("Multi-Tasks Bot - nn!"))
 
-# help message
+# general cog
+class General:
+    @bot.command()
+    async def ping(ctx) :
+        """Check the ping time of the bot"""
+        await ctx.send(f"🏓 Pong with {str(round(bot.latency, 2))} seconds!")
 
-@bot.command()
-async def help(ctx):
-    helptext = "**Neno Bot**\nhttps://cdn.discordapp.com/app-icons/796854915850567720/ac273fdf92876b8d2e7945012a482829.png\nNeno is a multi-tasks bot made for help people.\n\n**Commands**:\n"
-    for command in self.bot.commands:
-        helptext+=f"**{command}**: \n{exec(command + '.__doc__')}\n\n"
-    await ctx.send(helptext)
+    @bot.command()
+    async def clear(ctx, amount=3) :
+        """Purge the indicated amount of messages (by default 3)"""
+        await ctx.channel.purge(limit=amount)
 
-# general
+    @bot.command()
+    async def check_url(ctx, url):
+        """Ping a URL for check his activity"""
+        try:
+            await ctx.send(f"The URL have return the code " + str(requests.get(url).status_code) + "!")
+        except:
+            await ctx.send(f"We have a problem with this URL... (Try to add http:// or https:// in the URL)")
+            
+# add general cog
+bot.add_cog(General())
 
-@bot.command()
-async def ping(ctx) :
-    """Check the ping time of the bot"""
-    await ctx.send(f"🏓 Pong with {str(round(bot.latency, 2))} seconds!")
-
-@bot.command()
-async def clear(ctx, amount=3) :
-    """Purge the indicated amount of messages (by default 3)"""
-    await ctx.channel.purge(limit=amount)
-
-@bot.command()
-async def check_url(ctx, url):
-    """Ping a URL for check his activity"""
-    try:
-        await ctx.send(f"The URL have return the code " + str(requests.get(url).status_code) + "!")
-    except:
-        await ctx.send(f"We have a problem with this URL... (Try to add http:// or https:// in the URL)")
-
+# start the bot
 bot.run(token)
