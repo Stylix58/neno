09d59d5d9f32381c04e228b74ce61720c25e3c1b
Stylix58 <63605602+Stylix58@users.noreply.github.com>
2021-01-08 17:46:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,42 @@ import requests
 from discord.ext import commands
 from os import getenv
 
-client = commands.Bot(command_prefix="nn!")
+bot = commands.Bot(command_prefix="nn!")
 token = getenv("DISCORD_TOKEN")
 
-@client.event
+@bot.event
 async def on_ready() :
-    await client.change_presence(status = discord.Status.online, activity = discord.Game("Multi-Tasks Bot - nn!"))
+    await bot.change_presence(status = discord.Status.online, activity = discord.Game("Multi-Tasks Bot - nn!"))
+
+# help message
+
+@bot.command(name="help", description="Returns all commands available")
+async def help(ctx):
+    helptext = """
+    **Neno Bot**
+    https://cdn.discordapp.com/app-icons/796854915850567720/ac273fdf92876b8d2e7945012a482829.png
+    Neno is a multi-tasks bot made for help people.
+    
+    **Commands**:
+    
+    """
+    for command, desc in self.bot.commands:
+        helptext+=f"**{command}**: \n{desc}\n\n"
+    await ctx.send(helptext)
 
 # general
 
-@client.command()
+@bot.command()
 async def ping(ctx) :
     """Check the ping time of the bot"""
-    await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
+    await ctx.send(f"🏓 Pong with {str(round(bot.latency, 2))} seconds!")
 
-@client.command()
+@bot.command()
 async def clear(ctx, amount=3) :
     """Purge the indicated amount of messages (by default 3)"""
     await ctx.channel.purge(limit=amount)
 
-@client.command()
+@bot.command()
 async def check_url(ctx, url):
     """Ping a URL for check his activity"""
     try:
@@ -30,4 +46,4 @@ async def check_url(ctx, url):
     except:
         await ctx.send(f"We have a problem with this URL... (Try to add http:// or https:// in the URL)")
 
-client.run(token)
+bot.run(token)
