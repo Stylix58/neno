00f6cbefbc6075847c42e6fa07472fbb56ce4ad6
Stylix58 <63605602+Stylix58@users.noreply.github.com>
2021-01-08 07:09:54 +0100

    ok

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ async def clear(ctx, amount=3) :
 @client.command()
 async def check_url(ctx, url):
     """Ping a URL for check his activity"""
+    await ctx.send(f"Pinging the URL...")
     await ctx.send(f"The URL have return the code " + str(requests.get(url).status_code) + "!")
 
 client.run(token)
